7a30eff137d163461a7144e4d3714cf95262aeb1
Kareem <karimsaid026@gmail.com>
2023-10-26 17:43:07 -0700

    Adding Create View with all validation and constraints and connecting it with the database

--- Employee CRUD/Employee CRUD/Controllers/EmployeeController.cs
+++ Employee CRUD/Employee CRUD/Controllers/EmployeeController.cs
@@ -3,14 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Employee_CRUD.Models;
 
 namespace Employee_CRUD.Controllers
 {
     public class EmployeeController : Controller
     {
+        private employeeContext DBContext = new employeeContext();
+
         public ActionResult Home()
         {
             return View();
         }
+
+        public ActionResult Create()
+        {
+            ViewBag.govs = new SelectList(DBContext.Governments.ToList(), "id", "name");
+            ViewBag.depts = new SelectList(DBContext.Departments.ToList(), "id", "name");
+            ViewBag.villages = new SelectList(DBContext.villages.ToList(), "id", "name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Department department = DBContext.Departments.Where(n => n.id == employee.dept_id).FirstOrDefault();
+                village village = DBContext.villages.Where(n => n.id == employee.village_id).FirstOrDefault();
+
+                if (department.gov_id == employee.gov_id)
+                {
+                    if (village.dept_id == employee.dept_id)
+                    {
+                        DBContext.Employees.Add(employee);
+                        DBContext.SaveChanges();
+                        return RedirectToAction("Home");
+                    }
+                    else
+                        ViewBag.villageErrorMsg = "The Department doesn't have the selected Village";
+                    
+                }
+                else
+                    ViewBag.deptErrorMsg = "The Goverment doesn't have the selected department";
+            }
+
+            ViewBag.govs = new SelectList(DBContext.Governments.ToList(), "id", "name");
+            ViewBag.depts = new SelectList(DBContext.Departments.ToList(), "id", "name");
+            ViewBag.villages = new SelectList(DBContext.villages.ToList(), "id", "name");
+            return View();
+        }
+
+
+
     }
 }
--- Employee CRUD/Employee CRUD/Models/Employee.cs
+++ Employee CRUD/Employee CRUD/Models/Employee.cs
@@ -9,30 +9,46 @@ namespace Employee_CRUD.Models
     [Table("Employee")]
     public partial class Employee
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "*Required")]
         [StringLength(50)]
+        [Display(Name="Name")]
         public string name { get; set; }
 
-        [Required]
-        [StringLength(14)]
+        [Required(ErrorMessage = "*Required")]
+        [StringLength(14, MinimumLength = 14, ErrorMessage = "The Employee National ID must be 14 Digit")]
+        [RegularExpression(@"[2-3][0-9]+", ErrorMessage = "The Employee National ID must start with 2 OR 3")]
+        [Display(Name = "National ID")]
         public string national_id { get; set; }
 
-        [Required]
-        [StringLength(11)]
+        [Required(ErrorMessage = "*Required")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "The Employee Phone Number must be 11 Digit")]
+        [RegularExpression(@"[0][1][0][0-9]+", ErrorMessage = "The Employee Phone Number must start with 010")]
+        [Display(Name = "Phone Number")]
         public string phone_number { get; set; }
 
+        [Required(ErrorMessage = "*Required")]
+        [Range(18, 60, ErrorMessage = "The Employee Age must be between 18 and 60")]
+        [Display(Name = "Age")]
         public int age { get; set; }
 
+        [Required(ErrorMessage = "*Required")]
+        [Display(Name = "Salary")]
         public double salary { get; set; }
 
+        [Display(Name = "Is married")]
         public bool married { get; set; }
 
+        [Display(Name = "Government")]
         public int? gov_id { get; set; }
 
+        [Display(Name = "Department")]
         public int? dept_id { get; set; }
 
+        [Display(Name = "Village")]
         public int? village_id { get; set; }
 
         public virtual Department Department { get; set; }
